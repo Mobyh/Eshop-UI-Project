b87343bf2f0baeb6528f6bf74415230df3249074
Mobyh <luisgaza95sant@gmail.com>
2020-04-15 01:06:40 -0500

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/brick.png" />
+    <todo></todo>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
